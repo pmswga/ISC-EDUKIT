41538ae53c2f6d69733daf194c00455fb46b7338
Sergey <pmswga@gmail.com>
2017-02-26 17:26:56 +0300

    Ограничения

--- sql/tables.sql
+++ sql/tables.sql
@@ -13,12 +13,11 @@ CREATE TABLE `users` (
 	password char(32) NOT NULL,
 	id_type_user int NOT NULL,
 	INDEX (id_type_user),
-	CONSTRAINT uc_sn CHECK(second_name <> NULL AND second_name <> ''),
-	CONSTRAINT uc_fn CHECK(first_name <> NULL AND first_name <> ''),
-	CONSTRAINT uc_pt CHECK(patronymic <> NULL AND patronymic <> ''),
-	CONSTRAINT uc_email CHECK(email <> NULL AND email <> ''),
-	CONSTRAINT uc_password CHECK(password <> NULL AND password <> ''),
-	CONSTRAINT uc_type_user CHECK(id_type_user <> NULL AND id_type_user > 0 AND id_type_user <> 0)
+	CONSTRAINT uc_sn CHECK(second_name <> ''),
+	CONSTRAINT uc_fn CHECK(first_name <> ''),
+	CONSTRAINT uc_pt CHECK(patronymic <> ''),
+	CONSTRAINT uc_email CHECK(email <> ''),
+	CONSTRAINT uc_password CHECK(password <> '')
 ) ENGINE = InnoDB	 CHARACTER SET = UTF8;
 
 /*
@@ -36,7 +35,7 @@ CREATE TABLE `users` (
 CREATE TABLE `typeUser` (
 	id_type_user int AUTO_INCREMENT PRIMARY KEY,
 	description char(30) NOT NULL,
-	CONSTRAINT tuc_desc CHECK(description <> NULL AND description <> '')
+	CONSTRAINT tuc_desc CHECK(description <> '')
 ) ENGINE = InnoDB CHARACTER SET = UTF8;
 
 INSERT INTO `typeUser` (`description`) VALUES ('ADMIN');
@@ -46,65 +45,69 @@ INSERT INTO `typeUser` (`description`) VALUES ('STUDENT');
 INSERT INTO `typeUser` (`description`) VALUES ('PARENT');
 
 /* Создание таблицы "Студенты" */
-CREATE TABLE `students` (
+CREATE TABLE IF NOT EXISTS `students` (
 	id_student int PRIMARY KEY,
 	home_address char(255) NOT NULL,
 	cell_phone char(30) NOT NULL,
 	grp int NOT NULL,
 	INDEX (grp),
-	CONSTRAINT sc_ha CHECK(home_address <> NULL AND home_address <> ''),
-	CONSTRAINT sc_cp CHECK(cell_phone <> NULL AND cell_phone <> ''),
-	CONSTRAINT sc_grp CHECK(grp <> NULL AND grp > 0)
+	CONSTRAINT sc_ha CHECK(home_address <> ''),
+	CONSTRAINT sc_cp CHECK(cell_phone <> '')
 ) ENGINE = InnoDB CHARACTER SET = UTF8;
 
 /* Создание таблицы "Группы" */
-CREATE TABLE `groups` (
+CREATE TABLE IF NOT EXISTS `groups` (
 	grp int AUTO_INCREMENT PRIMARY KEY,
 	description char(10) NOT NULL,
 	code_spec int NOT NULL,
 	is_budget boolean NOT NULL,
 	INDEX (code_spec),
-	CONSTRAINT gc_desc CHECK(description <> NULL AND description <> ''),
-	CONSTRAINT gc_cs CHECK(code_spec <> NULL AND code_spec > 0),
-	CONSTRAINT gc_is_budget CHECK(is_budget <> NULL)
+	CONSTRAINT gc_desc CHECK(description <> '')
 ) ENGINE = InnoDB CHARACTER SET = UTF8;
 
 /* Создание таблицы "Специальности" */
-CREATE TABLE `specialty` (
+CREATE TABLE IF NOT EXISTS `specialty` (
 	id_spec int AUTO_INCREMENT PRIMARY KEY,
 	code_spec char(10) NOT NULL UNIQUE,
 	description char(255) NOT NULL,
 	pdf_file char(255) NOT NULL,
-	CONSTRAINT sc_cs CHECK(code_spec <> NULL AND code_spec <> ''),
-	CONSTRAINT sc_desc CHECK(description <> NULL AND description <> ''),
-	CONSTRAINT sc_file CHECK(pdf_file <> NULL AND pdf_file <> '')
+	CONSTRAINT sc_cs CHECK(code_spec <> ''),
+	CONSTRAINT sc_desc CHECK(description <> ''),
+	CONSTRAINT sc_file CHECK(pdf_file <> '')
 ) ENGINE = InnoDB CHARACTER SET = UTF8;
 
 /* Создание таблицы "Родители" */
-CREATE TABLE `parents` (
-	id_parent int PRIMARY KEY,
-	age int(2) NOT NULL,
+CREATE TABLE IF NOT EXISTS `parents` (
+	id_parent int NOT NULL PRIMARY KEY,
+	age smallint NOT NULL,
 	education char(50) NOT NULL,
 	work_place char(255) NOT NULL,
 	post char(255) NOT NULL,
 	home_phone char(20) NOT NULL,
-	cell_phone char(20) NOT NULL
+	cell_phone char(20) NOT NULL,
+	CONSTRAINT pc_edu CHECK(education <> ''),
+	CONSTRAINT pc_wp CHECK(work_place <> ''),
+	CONSTRAINT pc_post CHECK(post <> ''),
+	CONSTRAINT pc_home_phone CHECK (home_phone <> ''),
+	CONSTRAINT pc_cell_phone CHECK (cell_phone <> '')
 ) ENGINE = InnoDB CHARACTER SET = UTF8;
 
 /* Создание таблицы "Родитель-ребёнок" */
-CREATE TABLE `parent_child` (
+CREATE TABLE IF NOT EXISTS `parent_child` (
 	id_parent int NOT NULL,
 	id_children int NOT NULL,
 	id_type_relation int NOT NULL,
 	INDEX (id_children),
 	INDEX (id_type_relation),
-	PRIMARY KEY (id_parent, id_children)
+	PRIMARY KEY (id_parent, id_children),
+	CONSTRAINT pcc_tr CHECK(id_type_relation <> NULL AND id_type_relation > 0)
 ) ENGINE = InnoDB CHARACTER SET = UTF8;
 
 /* Создание таблицы "Отношения" */
-CREATE TABLE `relations` (
+CREATE TABLE IF NOT EXISTS `relations` (
 	id_relation int AUTO_INCREMENT PRIMARY KEY,
-	description char(255) NOT NULL
+	description char(255) NOT NULL,
+	CONSTRAINT rc_desc CHECK(description <> '')
 ) ENGINE = InnoDB CHARACTER SET = UTF8;
 
 INSERT INTO `relations` (`description`) VALUES ('Мама');
@@ -115,29 +118,33 @@ INSERT INTO `relations` (`description`) VALUES ('Отчим');
 INSERT INTO `relations` (`description`) VALUES ('Не определён');
 
 /* Создание таблицы "Преподаватели" */
-CREATE TABLE `teachers` (
-	id_teacher int PRIMARY KEY,
-	info TEXT NOT NULL
+CREATE TABLE IF NOT EXISTS `teachers` (
+	id_teacher int NOT NULL PRIMARY KEY,
+	info TEXT NOT NULL,
+	CONSTRAINT tc_info CHECK(info <> NULL)
 ) ENGINE = InnoDB CHARACTER SET = UTF8;
 
 /* Создание таблицы "Новости" */
-CREATE TABLE `news` (
+CREATE TABLE IF NOT EXISTS `news` (
 	id_news int AUTO_INCREMENT PRIMARY KEY,
 	caption char(255) NOT NULL UNIQUE,
 	content text NOT NULL,
 	id_author int NOT NULL,
 	date_publication date NOT NULL,
-	INDEX (id_author)
+	INDEX (id_author),
+	CONSTRAINT nc_caption CHECK(caption <> ''),
+	CONSTRAINT nc_content CHECK(content <> '')
 ) ENGINE = InnoDB CHARACTER SET = UTF8;
 
 /* Создание таблицы "Предметы" */
-CREATE TABLE `subjects` (
+CREATE TABLE IF NOT EXISTS `subjects` (
 	id_subject int AUTO_INCREMENT PRIMARY KEY,
-	description char(255) NOT NULL
+	description char(255) NOT NULL,
+	CONSTRAINT sc_desc CHECK(description <> '')
 ) ENGINE = InnoDB CHARACTER SET = UTF8;
 
 /* Создание таблицы "Преподаватели-предметы" */
-CREATE TABLE `teacher_subjects` (
+CREATE TABLE IF NOT EXISTS `teacher_subjects` (
 	id_teacher int NOT NULL,
 	id_subject int NOT NULL,
 	INDEX (id_subject),
@@ -145,31 +152,34 @@ CREATE TABLE `teacher_subjects` (
 ) ENGINE = InnoDB CHARACTER SET = UTF8;
 
 /* Создание таблицы "Тесты" */
-CREATE TABLE `tests` (
+CREATE TABLE IF NOT EXISTS `tests` (
 	id_test int AUTO_INCREMENT PRIMARY KEY,
 	id_subject int NOT NULL,
 	id_teacher int NOT NULL,
 	INDEX(id_subject),
 	INDEX(id_teacher),
-	for_group char(255) NOT NULL,
-	caption char(255) NOT NULL UNIQUE
+	caption char(255) NOT NULL UNIQUE,
+	CONSTRAINT tc_caption CHECK(caption <> '')
 ) ENGINE = InnoDB CHARACTER SET = UTF8;
 
 /* Создание таблицы "Вопросы" */
-CREATE TABLE `questions` (
+CREATE TABLE IF NOT EXISTS `questions` (
 	id_question int AUTO_INCREMENT PRIMARY KEY,
 	id_test int NOT NULL,
 	INDEX(id_test),
 	question char(255) NOT NULL UNIQUE,
-	r_answer char(255) NOT NULL
+	r_answer char(255) NOT NULL,
+	CONSTRAINT qc_question CHECK(question <> ''),
+	CONSTRAINT qc_ranswer CHECK(r_answer <> '')
 ) ENGINE = InnoDB CHARACTER SET = UTF8;
 
 /* Создание таблицы "Ответы" */
-CREATE TABLE `answers` (
+CREATE TABLE IF NOT EXISTS `answers` (
 	id_answer int AUTO_INCREMENT PRIMARY KEY,
 	id_question int NOT NULL,
 	INDEX(id_question),
-	answer char(255) NOT NULL UNIQUE
+	answer char(255) NOT NULL UNIQUE,
+	CONSTRAINT ac_answer CHECK(answer <> '')
 ) ENGINE = InnoDB CHARACTER SET = UTF8;
 
 /* Создание таблицы "Ответов студентов на тесты" */
@@ -187,13 +197,17 @@ CREATE TABLE IF NOT EXISTS `student_answer` (
   `id_student_test` int(11) NOT NULL PRIMARY KEY,
   `id_question` int(11) NOT NULL,
   `answer` char(255) NOT NULL,
-  INDEX(id_question)
+  INDEX(id_question),
+  CONSTRAINT sac_answer CHECK(answer <> '')
 ) ENGINE = InnoDB CHARACTER SET = UTF8;
 
 /* Создание таблицы "Посещаемости" */
-CREATE TABLE `student_traffic` (
+CREATE TABLE IF NOT EXISTS `student_traffic` (
   `id_traffic` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `id_student` int(11) NOT NULL,
   `date_traffic` date NOT NULL,
   INDEX(id_student)
 ) ENGINE = InnoDB CHARACTER SET = UTF8;
+
+/* Создание таблицы "Группы-тесты" */
+
