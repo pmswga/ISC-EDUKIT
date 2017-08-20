93fbbdc7563766bcd73fe638fff53d4cfd931c2f
Sergey <pmswga@gmail.com>
2017-08-20 16:24:43 +0300

    fix 3
    add description for classes:
    - User
    - Group

--- iep/structures/group.class.php
+++ iep/structures/group.class.php
@@ -7,16 +7,18 @@
   use IEP\Structures\Specialty;
   
   /*!
+    \class Group
     \brief Класс, которые несёт в себе информацию об группе
     \author pmswga
     \version 1.0
     
-    Класс является сущность, в которую помещаются данные из базы данных
+    Класс представляет собой сущность, в которую помещаются данные из базы данных
     
   */
   
 	class Group
 	{
+    
     private $id;
 		private $number;
 		private $spec;
@@ -24,10 +26,16 @@
 		private $is_budget;
     
     /*!
-      \param[in] $number 
-      \param[in] $spec 
-      \param[in] $year_education 
-      \param[in] $is_budget 
+      \param[in] $number         - название группы
+      
+      \param[in] $spec           - специальность
+      \note Представляет собой объект класса Specialty
+      
+      \param[in] $year_education - год выпуска
+      \note Указывается в формате [год]/[год]
+      
+      \param[in] $is_budget      - тип группы 
+      \note 1 - бюджетная, 0 - коммерческая
     */
     
 		function __construct(string $number, $spec, string $year_education, int $is_budget = 1)
@@ -38,6 +46,31 @@
       $this->year_education = $year_education;
 			$this->is_budget = $is_budget;
 		}
+    
+    /*!
+      \param[in] $method - вызываемый метод
+      \param[in] $args   - аргументы вызываемого метода
+      \return Вызов существующего метода
+      
+      \warning
+      
+        \par
+        Конкретно в этом классе переопределён метод __call().
+        Это было сделано для того, чтобы убрать "паравозик" при вызове методов из объекта
+        
+        \par
+        Например, чтобы раньше получить код специальности, нужно было писать:
+          $group->getSpec()->getCode()
+        
+        \par
+        После переопределения метода _call() можно писать:
+          $group->getCode()
+        
+        \par
+        В обоих случаях мы получаем код специальности
+      
+        
+    */
     
     public function __call($method, $args)
     {
@@ -50,30 +83,56 @@
       }
     }
     
+    /*!
+      \param[in] $id - идентификатор группы
+      \note Идентификатор берётся из базы данных
+    */
+    
     public function setGroupID(int $id)
     {
       $this->id = $id;
     }
     
+    /*!
+      \return Идентификатор группы
+      \note Далее идентификатор используется для манипуляций с данными в базе данных
+    */
+    
     public function getGroupID() : int
     {
       return $this->id;
     }
-        
+    
+    /*!
+      \return Название группы
+    */
+    
 		public function getNumberGroup() : string
 		{
 			return $this->number;
 		}
+    
+    /*!
+      \return Объект типа Specialty
+    */
 		
 		public function getSpec()
 		{
 			return $this->spec;
 		}
 		
+    /*!
+      \return Возвращает год обучения
+    */
+    
     public function getYearEducation() : string
     {
       return $this->year_education;
     }
+        
+    /*!
+      \return Возвращает статус группы
+    */
     
 		public function getStatus() : int
 		{
--- iep/structures/user.class.php
+++ iep/structures/user.class.php
@@ -2,14 +2,37 @@
   declare(strict_types = 1);
 	namespace IEP\Structures;
 	
+  /*!
+    \class User
+    \brief Общий класс для всех пользователей
+    \author pmswga
+    \version 1.0
+    
+    \details 
+      Данный класс содержит в себе общую информацию об пользователе
+    
+  */
+  
 	class User
 	{
-		protected $sn;
-		protected $fn;
-		protected $pt;
-		protected $email;
-		protected $password;
-		protected $typeUser;
+		protected $sn;        ///< Фамилия
+		protected $fn;        ///< Имя
+		protected $pt;        ///< Отчество
+		protected $email;     ///< Почта
+		protected $password;  ///< Пароль
+		protected $typeUser;  ///< Тип пользователя
+    
+    /*!
+      \param[in] $sn       - Фамилия
+      \param[in] $fn       - Имя
+      \param[in] $pt       - Отчество
+      \param[in] $email    - Электронная почта
+      \param[in] $password - Пароль
+      
+      \param[in] $typeUser - Тип пользователя
+      \note
+        там их много крч....
+    */
 		
 		function __construct(string $sn, string $fn, string $pt, string $email, string $password, int $typeUser = 0)
 		{
@@ -21,37 +44,61 @@
 			$this->typeUser = $typeUser;
 		}
     
+    /*!
+      \return Фамилию пользователя
+    */
+    
 		public function getSn() : string
 		{
 			return $this->sn;
 		}
+    
+    /*!
+      \return Имя пользователя
+    */
 		
 		public function getFn() : string
 		{
 			return $this->fn;
 		}
+    
+    /*!
+      \return Отчество пользователя
+    */
 		
 		public function getPt() : string
 		{
 			return $this->pt;
 		}
+    
+    /*!
+      \return Электронную почту пользователя
+    */
 		
 		public function getEmail() : string
 		{
 			return $this->email;
 		}
+    
+    /*!
+      \return Пароль пользователя
+      \note Пароль захеширован с помощью MD5
+    */
 		
 		public function getPassword() : string
 		{
 			return $this->password;
 		}
-		
+    
+    /*!
+      \return Вовзращает тип пользователя
+    */
+    
 		public function getUserType() : int
 		{
 			return $this->typeUser;
 		}
 		
 	}
-	
 	
 ?>
