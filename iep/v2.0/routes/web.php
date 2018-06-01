9a7643574de2bc65725e974d29f1a8d0e7c4aa5c
Sergey <pmswga@gmail.com>
2018-06-01 21:18:43 +0300

    Работа над порталом
    - Настройка подключения базы данных
    - Создание базовой авторизации Laravel

--- iep/v2.0/routes/web.php
+++ iep/v2.0/routes/web.php
@@ -11,6 +11,13 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Auth::routes();
+Route::group(["/"], function () {
+
+    Route::get('/', function () {
+        return view('welcome');
+    });
+
+    Route::get('/home', 'HomeController@index')->name('home');
+
 });
