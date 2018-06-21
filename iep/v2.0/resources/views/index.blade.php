b94b5c2619fcaba39314c6f5c724721e44dcb2a7
Sergey <pmswga@gmail.com>
2018-06-21 15:35:36 +0300

    Работа над документами
    - Корректировка пояснительной записки
    - Написание руководство пользователя
    - Корректировка рецензии

--- iep/v2.0/resources/views/index.blade.php
+++ iep/v2.0/resources/views/index.blade.php
@@ -3,26 +3,6 @@
 @section('title', 'Главная страница')
 
 @section('content')
-    <fieldset>
-        <legend>Объявления</legend>
-        <div class="ui three column grid">
-            <div class="column">
-                <div class="ui piled red segment">
-                    <h3>Важные</h3>
-                </div>
-            </div>
-            <div class="column">
-                <div class="ui piled orange segment">
-                    <h3>Обычные</h3>
-                </div>
-            </div>
-            <div class="column">
-                <div class="ui piled blue segment">
-                    <h3>Информационные</h3>
-                </div>
-            </div>
-        </div>
-    </fieldset>
     <h1 align="center">Добро пожаловать</h1>
         <hr>
         <blockquote>
