c517bf0d501b33641d60a2f37726cfe65e37d1a5
Sergey <pmswga@gmail.com>
2018-06-23 01:47:08 +0300

    Работа над порталом

--- iep/v2.0/resources/views/auth/passwords/email.blade.php
+++ iep/v2.0/resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,42 @@
+
 @extends('layouts.app')
 
+@section('title', 'Восстановление пароля')
+
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Reset Password</div>
-
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form class="form-horizontal" method="POST" action="{{ route('password.email') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Send Password Reset Link
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+    <div class="ui segment">
+        @if ($errors->any())
+            <div class="ui error message">
+                <i class="close icon"></i>
+                <div class="header">
+                    Ошибка аутентификации
                 </div>
+                <ul class="list">
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
             </div>
-        </div>
+        @endif
+        @if (session('status'))
+            <div class="ui positive message">
+                {{ session('status') }}
+            </div>
+        @endif
+        <form class="ui form" method="POST" action="{{ route('password.email') }}">
+            {{ csrf_field() }}
+
+            <div class="field">
+                <label for="email">E-Mail</label>
+                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+
+            </div>
+
+            <div class="field">
+                <button type="submit" class="ui primary button">
+                    Восстановить пароль
+                </button>
+            </div>
+        </form>
     </div>
-</div>
 @endsection
