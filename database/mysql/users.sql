7977cd0492a116247d7f769d0442c4c3c5efada9
Sergey <pmswga@gmail.com>
2018-06-24 23:00:02 +0300

    Подготовка ветки разработки

--- database/mysql/users.sql
+++
@@ -1,8 +0,0 @@
-
-DROP USER IF EXISTS `iep`@`localhost`;
-
-CREATE USER `iep`@`localhost`;
-
-GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE, REFERENCES ON iep.* TO `iep`@`localhost`;
-
-SET PASSWORD FOR 'iep'@'localhost' = PASSWORD('#include <iostream> using namespace std; int main(int argc, char *argv[]) { cout << "Hello World";}');
--- iep/v1.0/admin/php/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-  require_once "../start.php";
-
-	unset($_SESSION['admin']);
-	CTools::Redirect("../login.php");
-?>
