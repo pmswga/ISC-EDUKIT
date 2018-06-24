900cb35266148532460d0d7983e5d25303bb94eb
Sergey <pmswga@gmail.com>
2018-06-24 23:27:50 +0300

    Удаление файлов второй версии портала

--- iep/v2.0/resources/lang/en/reg.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'first_name_field'     => 'Name',
-    'email_field'          => "E-Mail",
-    'passwd_field'         => 'Password',
-    'confirm_passwd_field' => "Retry password",
-    'submit_field' => "Registration",
-];
